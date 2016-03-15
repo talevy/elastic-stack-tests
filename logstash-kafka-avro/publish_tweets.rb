2118c0446e599ce1bd916b11b2c386ef2300c56b
Tal Levy <jubboy333@gmail.com>
2016-03-15 02:27:05 -0700

    first try at a vagga test for logstash+kafka+avro

---
+++ logstash-kafka-avro/publish_tweets.rb
@@ -0,0 +1,32 @@
+require 'avro'
+require 'poseidon'
+
+TWEPOCH = 1288834974657
+SCHEMA = Avro::Schema.parse(File.read("twitter.avsc"))
+
+def gen_random_events(length=100)
+  time = TWEPOCH
+  events = []
+  length.times {
+    events << {"username" => "name", "tweet" => "I have things to say!", "timestamp" => time}
+    time += Random.new.rand(1..300)
+  }
+  events
+end
+
+def encode(event)
+  dw = Avro::IO::DatumWriter.new(SCHEMA)
+  buffer = StringIO.new
+  encoder = Avro::IO::BinaryEncoder.new(buffer)
+  dw.write(event, encoder)
+  buffer.string
+end
+
+N = 1000
+N_BATCH = 100
+topic = "avro_meet_kafka"
+producer = Poseidon::Producer.new(["localhost:9092"], "my_test_producer")
+gen_random_events(N).each_slice(N_BATCH) { |batch|
+  producer.send_messages(batch.map{|m| Poseidon::MessageToSend.new(topic, encode(m))})
+}
+producer.shutdown
